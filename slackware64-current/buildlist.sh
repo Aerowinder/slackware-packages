95571a56bdcf924e760b2ac109f0e291f6d2a080
Aerowinder <Aerowinder@gmail.com>
2026-01-04 16:18:51 -0500

    formatting updates

--- slackware64-current/buildlist.sh
+++ slackware64-current/buildlist.sh
@@ -21,17 +21,17 @@ CHECKFILE=CHECKSUMS.md5
 SIZEOP=""                # Use bytes for sizes
 
 if test "$1" == ""; then
-        echo "Usage: $0 directory listfile(optional)"
-        exit
+    echo "Usage: $0 directory listfile(optional)"
+    exit
 else
-        if test ! -d $1; then
-                echo "Error: $1 - No such directory..."
-                exit
-        fi;
+    if test ! -d $1; then
+        echo "Error: $1 - No such directory..."
+        exit
+    fi;
 fi;
 
 if test "$2" != ""; then
-        LISTFILE=$2
+    LISTFILE=$2
 fi;
 
 echo "Building \"${LISTFILE}\" from \"$1\""
@@ -44,10 +44,10 @@ Directory listing of $1
 EOF
 
 find ${1} -exec ls -dl${SIZEOP} --full-time {} \; \
-        | awk '{ printf("%10s %2s %4s %4s %8s %10s %-5.5s %s\n",$1,$2,$3,$4,$5,$6,$7,$9) }' > templist.txt
+    | awk '{ printf("%10s %2s %4s %4s %8s %10s %-5.5s %s\n",$1,$2,$3,$4,$5,$6,$7,$9) }' > templist.txt
 
 if [ -f ${LISTFILE} ]; then
-  rm -f ${LISTFILE}
+    rm -f ${LISTFILE}
 fi
 #sort +7d templist.txt >> ${LISTFILE}
 sort -k 7d templist.txt >> ${LISTFILE}
@@ -56,7 +56,7 @@ sed -i '/templist.txt/d' ${LISTFILE}
 sed -i '/buildlist.sh/d' ${LISTFILE}
 
 if [ -f ${CHECKFILE} ]; then
-  rm -f ${CHECKFILE}
+    rm -f ${CHECKFILE}
 fi
 find ${1} -exec md5sum {} \; >> ${CHECKFILE}
 
